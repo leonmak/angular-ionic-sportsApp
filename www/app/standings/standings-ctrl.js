a33dd6699d170ba0506880a9f91832f749156bcf
leon mak <leonmakansheng@gmail.com>
2015-12-09 13:14:37 +0800

    added list dividers for standings and teams divisions

---
+++ www/app/standings/standings-ctrl.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  angular.module('eliteApp').controller('StandingsCtrl',['eliteApi',StandingsCtrl]);
+  function StandingsCtrl(eliteApi){
+    var vm   = this;
+    var data = eliteApi.getLeagueData();
+    vm.standings = data.standings;
+  }
+})();
